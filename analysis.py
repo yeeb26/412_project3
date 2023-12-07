e44a7abd1b8d86397468b564656a0e35ea557106
yeeb26 <89863204+yeeb26@users.noreply.github.com>
2023-12-07 13:16:27 -0600

    Update analysis.py
    question 8 complete

--- analysis.py
+++ analysis.py
@@ -60,10 +60,10 @@ def question3e():
     # If not possible, return 'NOT POSSIBLE'
 
 def question8():
-    answerEpsilon = None
-    answerLearningRate = None
-    return answerEpsilon, answerLearningRate
-    # If not possible, return 'NOT POSSIBLE'
+    answerEpsilon = 0.8
+    answerLearningRate = 0.2
+    # return answerEpsilon, answerLearningRate
+    return 'NOT POSSIBLE'
 
 if __name__ == '__main__':
     print('Answers to analysis questions:')
